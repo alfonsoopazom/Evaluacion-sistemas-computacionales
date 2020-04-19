894ce140617b4cce2c41d021584545ad1ed7fad0
Alfonzo! <32660804+alfonsoopazo@users.noreply.github.com>
2020-04-19 14:26:08 -0400

    Cambios steticos archivo usuario

--- usuario.php
+++ usuario.php
@@ -4,189 +4,57 @@
     <meta charset="utf-8">
     <title>Usuario</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
-        body{
-    background: -webkit-linear-gradient(left, #3931af, #00c6ff);
-}
-.emp-profile{
-    padding: 3%;
-    margin-top: 3%;
-    margin-bottom: 3%;
-    border-radius: 0.5rem;
-    background: #fff;
-}
-.profile-img{
-    text-align: center;
-}
-.profile-img img{
-    width: 70%;
-    height: 100%;
-}
-.profile-img .file {
-    position: relative;
-    overflow: hidden;
-    margin-top: -20%;
-    width: 70%;
-    border: none;
-    border-radius: 0;
-    font-size: 15px;
-    background: #212529b8;
-}
-.profile-img .file input {
-    position: absolute;
-    opacity: 0;
-    right: 0;
-    top: 0;
-}
-.profile-head h5{
-    color: #333;
-}
-.profile-head h6{
-    color: #0062cc;
-}
-.profile-edit-btn{
-    border: none;
-    border-radius: 1.5rem;
-    width: 70%;
-    padding: 2%;
-    font-weight: 600;
-    color: #6c757d;
-    cursor: pointer;
-}
-.proile-rating{
-    font-size: 12px;
-    color: #818182;
-    margin-top: 5%;
-}
-.proile-rating span{
-    color: #495057;
-    font-size: 15px;
-    font-weight: 600;
-}
-.profile-head .nav-tabs{
-    margin-bottom:5%;
-}
-.profile-head .nav-tabs .nav-link{
-    font-weight:600;
-    border: none;
-}
-.profile-head .nav-tabs .nav-link.active{
-    border: none;
-    border-bottom:2px solid #0062cc;
-}
-.profile-work{
-    padding: 14%;
-    margin-top: -15%;
-}
-.profile-work p{
-    font-size: 12px;
-    color: #818182;
-    font-weight: 600;
-    margin-top: 10%;
-}
-.profile-work a{
-    text-decoration: none;
-    color: #495057;
-    font-weight: 600;
-    font-size: 14px;
-}
-.profile-work ul{
-    list-style: none;
-}
-.profile-tab label{
-    font-weight: 600;
-}
-.profile-tab p{
-    font-weight: 600;
-    color: #0062cc;
-}
-    </style>
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <link href="css/estilosUsuarios.css" rel="stylesheet">
+    
 </head>
 <body>
-<div class="container emp-profile">
-            <form method="post">
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-img">
-                            <img src="https://vignette.wikia.nocookie.net/coraje-el-perro-cobarde/images/a/a6/226px-Coraje_elperro.jpg/revision/latest?cb=20121009214558&path-prefix=es" alt=""/>
-                            <div class="file btn btn-lg btn-primary">
-                                Cambiar Foto
-                                <input type="file" name="file"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="profile-head">
-                            <h3>
-                                Rinaldo Fuentes
-                            </h3>
-                            <h6>
-                                Usuario Blog ESC
-                            </h6>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Editar Perfil"/>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-work">
-                            <p>REDES SOCIALES</p>
-                            <a href="">Facebook</a><br/>
-                            <a href="">Instagram</a><br/>
-                            <a href="">Twitter</a>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                      <div>
-                          <h4>
-                            <b>
-                              INFORMACIÓN
-                            </b>
-                          </h4>
-                      </div>
-                        <div class="tab-content profile-tab" id="myTabContent">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Usuario</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>Ejemplo</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Nombre</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>...........</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>blablabla@gmail.com</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Biografía</label>
-                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-<script type="text/javascript">
 
-</script>
+<div class="container">
+    <div class="box1">
+        <div class ="box1 img">
+            <img src="imagenes/logoUcen.png" alt=" no se encuentra la imagen"/>
+            <div class="cambiar-img">
+                Cambiar foto
+                <input type="text" name="text">
+            </div>
+            
+        </div>
+        <div class="bio">
+            <h4 class=" bio biografia">
+                Información general
+            </h4>
+            <div class="datos">
+                <p>Usuario</p><br>
+                <p>Correo</p><br>
+                <p>Nombre</p><br>
+            </div>
+        </div>  
+    </div>
+    <div class="titulo">
+        <h1>Nombre del usuario</h1>
+        <a>Estudiante de ESC</a>
+        <div class=" titulo info">
+            <p>Rinaldo-Fuentes-Souza</p><br>
+            <p>rinaldofuentesdasilva@brasil.com</p><br>
+            <p>Rinaldo Fuentes</p><br>
+        </div>
+    </div>
+    <div class="volverHome" aling="justify">
+        <a class="" href="#" id=""> Volver
+           
+        </a>
+        <a href="#" id="volver">
+            <button class="" onclick="">Editar</button>
+        </a>
+        <div class="volver-biografia">
+            <h3>Biografia</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint odit maxime aut ab, est deserunt, eveniet fugit, natus reprehenderit placeat facere necessitatibus voluptas vel illum repudiandae sapiente facilis nihil magni!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, libero aliquid? Suscipit at, vero iusto deserunt nobis iste laborum ratione, vel est accusamus unde, quam aut! Nisi excepturi vero recusandae?</p>
+        </div>
+    </div>
+    
+       
+</div>
+
 </body>
 </html>
