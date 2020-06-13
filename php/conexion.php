315f47108f3cd22f2c4c7bea5cca3f868104eb1e
Alfonzo! <32660804+alfonsoopazo@users.noreply.github.com>
2020-06-13 12:14:53 -0400

    conexion a mysql Azure
    Se agrega el primer intento de conexion con el motor de base de datos Mysql en Azure.

--- php/conexion.php
+++ php/conexion.php
@@ -1,18 +1,34 @@
-<?php
+    <?php
 
-    $host="localhost";
-    $userName="root";
-    $db = "blog";
+    $host="esc1.mysql.database.azure.com";
+    $userName="AlfonsoOpazo@esc1";
+    $db = "Blog";
+    $pass = "Alfonzo2020";
+    $Port=3306;
+
+    //Conexion a mysql mediante Azure
+    $con=mysqli_init(); 
+    //mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
+    mysqli_real_connect($con, $host, $userName, $pass, $db, $Port);
+
+
+    if (mysqli_connect_errno($con)) {
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }
+
+    //$con=mysqli_init(); 
+    //mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
+    //mysqli_real_connect($con, "esc1.mysql.database.azure.com", "AlfonsoOpazo@esc1", {your_password}, {your_database,3306);
 
     //Crear conexion
-    $conexion = mysqli_connect($host,$userName,"",$db); 
+    //$conexion = mysqli_connect($host,$userName,$pass,$db); 
 
     //verificar conexion
-    if ($conexion->connect_error) {
-        die("Connection failed: " . $conexion->mysqli_connect_error());
-    } else{
+    //if ($conexion->connect_error) {
+      //  die("Connection failed: ".$conexion->mysqli_connect_error());
+    //} else{
         //echo "Conexion correcta"; 
-    }
+    //}
      
 
 ?>
